d4ec2cd67ca0031b2c17df012245b6aa63de13a2
Martin Beechen <martinb@doordash.com>
2018-06-30 02:13:51 -0700

    Some examples of onclicklistener

--- app/src/main/java/com/brazenhiker/richcalc/MainHostActivity.java
+++ app/src/main/java/com/brazenhiker/richcalc/MainHostActivity.java
@@ -1,8 +1,6 @@
 package com.brazenhiker.richcalc;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -15,14 +13,6 @@ public class MainHostActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setTitle(R.string.main_host_activity_title);
-        loadFragment();
-    }
-
-    private void loadFragment() {
-        Fragment fragment = MainHostActivityFragment.newInstance();
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.fragment, fragment);
-        transaction.commit();
     }
 
 }
--- app/src/main/java/com/brazenhiker/richcalc/MainHostActivityFragment.java
+++ app/src/main/java/com/brazenhiker/richcalc/MainHostActivityFragment.java
@@ -1,15 +1,23 @@
 package com.brazenhiker.richcalc;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainHostActivityFragment extends Fragment {
+
+    TextView readOutText;
+    Button oneButton;
+    Button twoButton;
 
     public static MainHostActivityFragment newInstance() {
         return new MainHostActivityFragment();
@@ -21,5 +29,22 @@ public class MainHostActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main_host, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        readOutText = getView().findViewById(R.id.readOutText);
+
+        oneButton = getView().findViewById(R.id.one_button);
+        oneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readOutText.setText("1");
+            }
+        });
+
+        twoButton = getView().findViewById(R.id.two_button);
+        twoButton.setOnClickListener(v -> readOutText.setText("2"));
     }
 }
